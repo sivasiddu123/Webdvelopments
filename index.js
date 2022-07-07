e3915afc432e54e5ddc5489e57140a9d5229d4d9
siddu <105799401+sivasiddu123@users.noreply.github.com>
2022-07-07 19:19:38 +0530

    ok

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-let oil=50;
-console.log(oil);
+// var s1=90;
+// var s2=96;
+// if (s1<=s2 || s1==s2){
+// console.log("hello");--------------boolean operaters (&& ||)
+// }
+// else{
+//     console.log("no man");
+// }
 
-oil=oil+35;
-console.log(oil);
+// console.log(1>2 ? true:false);---------terinary operaters
+var weight=parseFloat(prompt("enter your age"));
+switch(weight){
+    
+    case2:console.log("weight is 2");
+    break;
+    case3:console.log("weight is 3");
+    break;
 
-oil=oil+100;
-console.log(oil);
-
+}
